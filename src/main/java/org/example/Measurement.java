b1291b8ddbbaaf448aa19453956a847721548356
Himanshu Maharshi <himanshu.m_ftc@external.swiggy.in>
2024-09-19 13:15:16 +0530

    added subtraction() and compare() functionality

--- src/main/java/org/example/Measurement.java
+++ src/main/java/org/example/Measurement.java
@@ -10,7 +10,7 @@ public class Measurement {
 
 
     public Measurement(double value, Unit unit) throws Exception {
-        if (value <= 0) {
+        if (value < 0) {
             throw new Exception("Input should be a non zero positive value");
         }
         this.value = value;
@@ -25,22 +25,43 @@ public class Measurement {
         return new Measurement(toUnit.fromBaseUnit() * valueInBaseUnit, toUnit);
     }
 
-    public Measurement add(Measurement otherUnit, Unit resultUnit) throws Exception {
-        if (this.unit.getUnitType() != resultUnit.getUnitType() && this.unit.getUnitType() != otherUnit.unit.getUnitType()) {
+    public Measurement add(Measurement additionAmount) throws Exception {
+        if (this.unit.getUnitType() != additionAmount.unit.getUnitType()) {
             throw new Exception("Cannot convert between different Units");
         }
 
-        // convert both units to base unit
-        double currentValueInBaseUnit = unit.toBaseUnit() * value;
-        double otherUnitInBaseUnit = otherUnit.unit.toBaseUnit() * otherUnit.value;
+        // convert additionAmount into base unit
+        double additionAmountInBaseUnit = additionAmount.unit.toBaseUnit() * additionAmount.value;
 
-        // sum in base unit
-        double sumInBaseUnit = currentValueInBaseUnit + otherUnitInBaseUnit;
+        // result
+        double resultantSumValue = (unit.toBaseUnit() * value + additionAmountInBaseUnit) * this.unit.fromBaseUnit();
 
-        // convert the sum to target unit
-        double resultValue = sumInBaseUnit * resultUnit.fromBaseUnit();
+        return new Measurement(resultantSumValue, this.unit);
+    }
 
-        return new Measurement(resultValue, resultUnit);
+    public Measurement sub(Measurement subtractionAmount) throws Exception {
+        if (this.unit.getUnitType() != subtractionAmount.unit.getUnitType()) {
+            throw new Exception("Cannot convert between different Units");
+        }
+
+        // convert subtractionAmount into base unit
+        double subtractionAmountInBaseUnit = subtractionAmount.unit.toBaseUnit() * subtractionAmount.value;
+
+        // result
+        double resultantSubValue = (unit.toBaseUnit() * value - subtractionAmountInBaseUnit) * this.unit.fromBaseUnit();
+
+        return new Measurement(resultantSubValue, this.unit);
+    }
+
+    public int compare(Measurement compareTo) throws Exception {
+        if (this.unit.getUnitType() != compareTo.unit.getUnitType()) {
+            throw new Exception("Cannot convert between different Units");
+        }
+
+        // convert compareTo unit value into base unit value
+        Measurement convertCompareToInBaseUnit = compareTo.convert(this.unit);
+
+        return Double.compare(this.value, convertCompareToInBaseUnit.value);
     }
 
     @Override
--- src/test/java/org/example/MeasurementTest.java
+++ src/test/java/org/example/MeasurementTest.java
@@ -96,11 +96,6 @@ class MeasurementTest {
     }
 
     @Test
-    void testInputCannotBe0Value() throws Exception {
-        assertThrows(Exception.class, () -> new Measurement(0.0, Unit.L));
-    }
-
-    @Test
     void testInputCannotBeNegative() throws Exception {
         assertThrows(Exception.class, () -> new Measurement(-9.0,  Unit.L));
     }
@@ -157,6 +152,18 @@ class MeasurementTest {
         assertEquals("Different types of measurements", exception.getMessage());
     }
 
+    // ------------------------------- unit test to add two units -------------------------------
+    // unit test to check how function responds to different Measurement types of addition
+    @Test
+    void testAddOneLiterToOneCentimeters() throws Exception {
+        Measurement liter = new Measurement(1.0, Unit.L);
+        Measurement centimeters = new Measurement(1.0, Unit.CM);
+
+        Exception exception = assertThrows(Exception.class, () -> {liter.add(centimeters);});
+
+        assertEquals("Cannot convert between different Units", exception.getMessage());
+    }
+
     @Test
     void testAddOneMilliliterToOneLiter() throws Exception {
         Measurement expected = new Measurement(1.001, Unit.L);
@@ -164,7 +171,31 @@ class MeasurementTest {
         Measurement liter = new Measurement(1.0, Unit.L);
         Measurement milliliter = new Measurement(1.0, Unit.ML);
 
-        Measurement actual = liter.add(milliliter, Unit.L);
+        Measurement actual = liter.add(milliliter);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAddOneDeciliterToTwoThousandMilliliter() throws Exception {
+        Measurement expected = new Measurement(2100.0, Unit.ML);
+
+        Measurement deciliter = new Measurement(1.0, Unit.DL);
+        Measurement milliliter = new Measurement(2000.0, Unit.ML);
+
+        Measurement actual = milliliter.add(deciliter);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAddFiveThousandMilliliterToTwoCentiliter() throws Exception {
+        Measurement expected = new Measurement(501., Unit.CL);
+
+        Measurement milliliter = new Measurement(5000.0, Unit.ML);
+        Measurement centiliter = new Measurement(1.0, Unit.CL);
+
+        Measurement actual = centiliter.add(milliliter);
 
         assertEquals(expected, actual);
     }
@@ -176,7 +207,19 @@ class MeasurementTest {
         Measurement kilometer = new Measurement(1.0, Unit.KM);
         Measurement meter = new Measurement(500.0, Unit.M);
 
-        Measurement actual = kilometer.add(meter, Unit.KM);
+        Measurement actual = kilometer.add(meter);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAdd2KilometerToOneThousandMillimeters() throws Exception {
+        Measurement expected = new Measurement(2000500.0, Unit.MM);
+
+        Measurement kilometer = new Measurement(2.0, Unit.KM);
+        Measurement millimeter = new Measurement(500.0, Unit.MM);
+
+        Measurement actual = millimeter.add(kilometer);
 
         assertEquals(expected, actual);
     }
@@ -188,19 +231,169 @@ class MeasurementTest {
         Measurement kilogram = new Measurement(1, Unit.KG);
         Measurement gram = new Measurement(700.0, Unit.G);
 
-        Measurement actual = kilogram.add(gram, Unit.KG);
+        Measurement actual = kilogram.add(gram);
 
         assertEquals(expected, actual);
     }
 
-    // unit test to check how function responds to different conversion types
     @Test
-    void testAddOneLiterToOneCentimeters() throws Exception {
+    void testAddSevenKilogramToOneThousandGrams() throws Exception {
+        Measurement expected = new Measurement(8000.0, Unit.G);
+
+        Measurement kilogram = new Measurement(7, Unit.KG);
+        Measurement gram = new Measurement(1000.0, Unit.G);
+
+        Measurement actual = gram.add(kilogram);
+
+        assertEquals(expected, actual);
+    }
+
+    // ------------------------------- unit test to subtract two units -------------------------------
+    // unit test to check how function responds to different Measurement types of subtraction
+    @Test
+    void testSubOneLiterToOneCentimeters() throws Exception {
         Measurement liter = new Measurement(1.0, Unit.L);
         Measurement centimeters = new Measurement(1.0, Unit.CM);
 
-        Exception exception = assertThrows(Exception.class, () -> {liter.add(centimeters, Unit.CM);});
+        Exception exception = assertThrows(Exception.class, () -> {liter.sub(centimeters);});
 
         assertEquals("Cannot convert between different Units", exception.getMessage());
+    }
+
+    @Test
+    void testSubOneMilliliterFromOneLiter() throws Exception {
+        Measurement expected = new Measurement(0.5, Unit.L);
+
+        Measurement liter = new Measurement(1.0, Unit.L);
+        Measurement milliliter = new Measurement(500, Unit.ML);
+
+        Measurement actual = liter.sub(milliliter);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubFiveHundredMetersFromOneKilometers() throws Exception {
+        Measurement expected = new Measurement(0.5, Unit.KM);
+
+        Measurement kilometer = new Measurement(1.0, Unit.KM);
+        Measurement meter = new Measurement(500.0, Unit.M);
+
+        Measurement actual = kilometer.sub(meter);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubHalfKilometersFromTwoThousandMeters() throws Exception {
+        Measurement expected = new Measurement(0.5, Unit.KM);
+
+        Measurement kilometer = new Measurement(1.0, Unit.KM);
+        Measurement meter = new Measurement(500.0, Unit.M);
+
+        Measurement actual = kilometer.sub(meter);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubTwoKilometersFromFiveThousandMeters() throws Exception {
+        Measurement expected = new Measurement(3000.0, Unit.M);
+
+        Measurement kilometer = new Measurement(2.0, Unit.KM);
+        Measurement meter = new Measurement(5000.0, Unit.M);
+
+        Measurement actual = meter.sub(kilometer);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubTenThousandMillimetersFromFiveThousandMeters() throws Exception {
+        Measurement expected = new Measurement(4990.0, Unit.M);
+
+        Measurement millimeter = new Measurement(10000.0, Unit.MM);
+        Measurement meter = new Measurement(5000.0, Unit.M);
+
+        Measurement actual = meter.sub(millimeter);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubTenKilogramFromFifteenThousandGrams() throws Exception {
+        Measurement expected = new Measurement(5000.0, Unit.G);
+
+        Measurement gram = new Measurement(15000.0, Unit.G);
+        Measurement kilogram = new Measurement(10.0, Unit.KG);
+
+        Measurement actual = gram.sub(kilogram);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubTenThousandMilligramFromTenGrams() throws Exception {
+        Measurement expected = new Measurement(0.0, Unit.G);
+
+        Measurement gram = new Measurement(10.0, Unit.G);
+        Measurement milligram = new Measurement(10000.0, Unit.MG);
+
+        Measurement actual = gram.sub(milligram);
+
+        assertEquals(expected, actual);
+    }
+
+    // ------------------------------- unit test to subtract two units -------------------------------
+    /*
+        1 -> calling object's value is greater than the argument
+        0 -> calling object's value is equal to the argument
+       -1 -> calling object's value is less than the argument
+    */
+    // unit test to check how function responds to different Measurement types of comparison
+    @Test
+    void testCompareOneLiterToOneCentimeters() throws Exception {
+        Measurement liter = new Measurement(1.0, Unit.L);
+        Measurement centimeters = new Measurement(1.0, Unit.CM);
+
+        Exception exception = assertThrows(Exception.class, () -> {liter.compare(centimeters);});
+
+        assertEquals("Cannot convert between different Units", exception.getMessage());
+    }
+
+    @Test
+    void testCompareOneLiterToOneThousandMilliliter() throws Exception {
+        int expected = 0; // 1000ML == 1.0L
+
+        Measurement milliliter = new Measurement(1000.0, Unit.ML);
+        Measurement liter = new Measurement(1.0, Unit.L);
+
+        int actual = milliliter.compare(liter);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCompareOneThousandFiveHundredGramToOneKilogram() throws Exception {
+        int expected = -1; // 1500G > 1KG
+
+        Measurement kilogram = new Measurement(1.0, Unit.KG);
+        Measurement gram = new Measurement(1500.0, Unit.G);
+
+        int actual = kilogram.compare(gram);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCompareOneMeterToOneHundredCentimeter() throws Exception {
+        int expected = 1; // 1.0M < 2000CM
+
+        Measurement centimeter = new Measurement(2000.0, Unit.CM);
+        Measurement meter = new Measurement(1.0, Unit.M);
+
+        int actual = centimeter.compare(meter);
+
+        assertEquals(expected, actual);
     }
 }
